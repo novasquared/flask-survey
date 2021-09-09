86dc1b52caf7b8bb452ffd82a64b7453a40e924d
Diogo <diogofilipebotelho@gmail.com>
2021-09-09 19:25:46 -0400

    Created show_question(), handle_question() and show_thank_you_message(). Refactored code to handle different surveys

--- app.py
+++ app.py
@@ -11,9 +11,51 @@ responses = []
 
 @app.get("/")
 def show_survey_details():
+    """Shows survey title and instructions and includes a button to start survey"""
     survey_title = survey.title
     survey_instructions = survey.instructions
     return render_template(
         "survey_start.html",
         survey_title=survey_title, 
         survey_instructions = survey_instructions)
+
+@app.post("/begin")
+def start_survey():
+    """Redirects user to the first question"""
+    return redirect("/questions/0")
+
+@app.get("/questions/<int:question_number_counter>")
+def show_question(question_number_counter):
+    """Shows question and choices"""
+    question = survey.questions[question_number_counter].question
+    choices = survey.questions[question_number_counter].choices
+
+    return render_template(
+        "question.html",
+        question = question,
+        choices = choices
+    )
+
+@app.post("/answer")
+def handle_response():
+    """Posts the response, redirects to next question or to 'thank you' message if survey is finished"""
+    # Get the response
+    #breakpoint()
+    response = request.form["answer"]
+    # Append response of question to responses list
+    responses.append(response)
+    # Check if this is the last question. If not, increment counter and redirect to next question
+    if (len(responses) < len(survey.questions)):
+        question_number_counter = len(responses)
+        return redirect(f"/questions/{question_number_counter}") #need to replace counter with something dynamic
+    #If yes, redirect to 'thank you' page
+    else:
+        return redirect("/thank-you")
+
+@app.get("/thank-you")
+def show_thank_you_message():
+    """Shows 'thank you' message"""
+    return render_template(
+        "completion.html"
+    )
+
