773a7e0215094141566f752efc98d2899c1901dc
jeanne <novasquared@yahoo.com>
2021-09-09 14:23:57 -0500

    modified survey_start.html and created the first route

--- app.py
+++ app.py
@@ -7,3 +7,13 @@ app.config['SECRET_KEY'] = "never-tell!"
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
+responses = []
+
+@app.get("/")
+def show_survey_details():
+    survey_title = survey.title
+    survey_instructions = survey.instructions
+    return render_template(
+        "survey_start.html",
+        survey_title=survey_title, 
+        survey_instructions = survey_instructions)
