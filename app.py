8ca7a1ed3e0650eac90466b9105459cd007e6e94
jeanne <novasquared@yahoo.com>
2021-09-09 19:48:44 -0500

    refactored code based on comments and updated some comments

--- app.py
+++ app.py
@@ -12,13 +12,10 @@ debug = DebugToolbarExtension(app)
 @app.get("/")
 def show_survey_details():
     """Shows survey title and instructions and includes a button to start survey"""
-    # dont need these since survey already passes use survey = survey in render and change html
-    # survey_title = survey.title
-    # survey_instructions = survey.instructions
     return render_template(
         "survey_start.html",
-        survey_title=survey_title, 
-        survey_instructions = survey_instructions)
+        survey = survey
+        )
 
 @app.post("/begin")
 def start_survey():
@@ -26,27 +23,29 @@ def start_survey():
     session["responses"] = []
     return redirect("/questions/0")
 
-@app.get("/questions/<int:question_number_counter>")
-def show_question(question_number_counter):
+@app.get("/questions/<int:question_number>")
+def show_question(question_number):
     """Shows question and choices"""
     responses = session["responses"]
-    if question_number_counter != len(responses):
+    if question_number != len(responses):
         flash("You are trying to access an invalid question.")
         return redirect(f"/questions/{len(responses)}")
 
-    question = survey.questions[question_number_counter].question
-    choices = survey.questions[question_number_counter].choices
+    question = survey.questions[question_number]
+    # question = survey.questions[question_number].question
+    # choices = survey.questions[question_number].choices
     # same ideas as show_survey details
     return render_template(
         "question.html",
-        question = question,
-        choices = choices
+        question=question
+        # question = question,
+        # choices = choices
     )
 
 @app.post("/answer")
 def handle_response():
     """Posts the response, redirects to next question or to 'thank you' message if survey is finished"""
-    # Get the response
+    # Get the response.  Request.form needs whatever is in name of the input and returns the value
     #breakpoint()
     response = request.form["answer"]
 
@@ -57,8 +56,8 @@ def handle_response():
 
     # Check if this is the last question. If not, increment counter and redirect to next question
     if len(responses) < len(survey.questions):
-        question_number_counter = len(responses)
-        return redirect(f"/questions/{question_number_counter}") #need to replace counter with something dynamic
+        question_number = len(responses)
+        return redirect(f"/questions/{question_number}")
     #If yes, redirect to 'thank you' page
     else:
         return redirect("/thank-you")
