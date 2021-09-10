7af75dbdfcc8f3ee1f7046fed25042b74012f6b1
jeanne <novasquared@yahoo.com>
2021-09-09 19:10:07 -0500

    added session, question protection and flash messaging functionality

--- app.py
+++ app.py
@@ -7,13 +7,14 @@ app.config['SECRET_KEY'] = "never-tell!"
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
-responses = []
+
 
 @app.get("/")
 def show_survey_details():
     """Shows survey title and instructions and includes a button to start survey"""
-    survey_title = survey.title
-    survey_instructions = survey.instructions
+    # dont need these since survey already passes use survey = survey in render and change html
+    # survey_title = survey.title
+    # survey_instructions = survey.instructions
     return render_template(
         "survey_start.html",
         survey_title=survey_title, 
@@ -22,14 +23,20 @@ def show_survey_details():
 @app.post("/begin")
 def start_survey():
     """Redirects user to the first question"""
+    session["responses"] = []
     return redirect("/questions/0")
 
 @app.get("/questions/<int:question_number_counter>")
 def show_question(question_number_counter):
     """Shows question and choices"""
+    responses = session["responses"]
+    if question_number_counter != len(responses):
+        flash("You are trying to access an invalid question.")
+        return redirect(f"/questions/{len(responses)}")
+
     question = survey.questions[question_number_counter].question
     choices = survey.questions[question_number_counter].choices
-
+    # same ideas as show_survey details
     return render_template(
         "question.html",
         question = question,
@@ -42,10 +49,14 @@ def handle_response():
     # Get the response
     #breakpoint()
     response = request.form["answer"]
+
     # Append response of question to responses list
+    responses = session["responses"]
     responses.append(response)
+    session["responses"] = responses
+
     # Check if this is the last question. If not, increment counter and redirect to next question
-    if (len(responses) < len(survey.questions)):
+    if len(responses) < len(survey.questions):
         question_number_counter = len(responses)
         return redirect(f"/questions/{question_number_counter}") #need to replace counter with something dynamic
     #If yes, redirect to 'thank you' page
